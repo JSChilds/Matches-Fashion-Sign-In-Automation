8a4ea6442f2f64e8e31c6e32e8cb63c11e3b7da4
Leebury <lbury@spartaglobal.com>
2017-08-03 12:12:31 +0100

    Currently goes to homepage

--- matches_homepage.rb
+++ matches_homepage.rb
@@ -1,4 +1,27 @@
 require 'watir'
 class MatchesHomepage
 
+	def initialize
+		@browser = Watir::Browser.new :chrome
+	end 
+
+	def go_to_homepage
+		@browser.goto 'http://www.matchesfashion.com'
+	end
+
+	def find_url
+		@browser.url
+	end
+
+	def sign_in_button
+		@browser.element(id: 'LoginLinks')		
+	end
+
+
 end
+
+
+homepage = MatchesHomepage.new
+
+homepage.go_to_homepage
+homepage.sign_in_button.click
